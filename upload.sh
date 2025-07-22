91480d3711883124a6623aa87227c70eb3bcaa0c
Glowman554 <vossjanick62@gmail.com>
2025-07-22 18:43:07 +0200

    fix upload script

--- upload.sh
+++ upload.sh
@@ -1,8 +1,14 @@
 make
 
-ftp -n <<EOF
+(
+    cd build_switch
+    make -j 8 OpenShock.nro
+    
+    ftp -n <<EOF
 open 10.100.100.102
 user anonymous
 cd /switch/
 put OpenShock.nro
-EOF⏎
+EOF
+
+)
